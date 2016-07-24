7a8aa40f6293c73d286527e54250843776b78ac2
Phillip Millwee <philmillwee2@gmail.com>
2016-07-24 19:44:38 -0400

    Added decodeTelemetry functionality

--- src/lib/packet.js
+++ src/lib/packet.js
@@ -1,7 +1,46 @@
 "use strict";
 
 const {join} = require("path");
-const raceMath = require(join(__dirname, "raceMath"));
+const {unsignedClamp, signedClamp} = require(join(__dirname, "raceMath"));
+const numeral = require("numeral");
+
+function decodeTelemetry(packet, callback) {
+  let telemetry = {
+    header: packet.header,
+    data: {
+      // Game States
+      mRaceState: (packet.data.sRaceStateFlags & 0x7),
+
+      // Unfiltered input
+      mUnfilteredThrottle: unsignedClamp(packet.data.sUnfilteredThrottle, 255),
+      mUnfilteredBrake: unsignedClamp(packet.data.sUnfilteredBrake, 255),
+      mUnfilteredSteering: signedClamp(packet.data.sUnfilteredSteering, 127),
+      mUnfilteredClutch: unsignedClamp(packet.data.sUnfilteredClutch, 255),
+
+      // Event Information
+      mLapsInEvent: packet.data.sLapsInEvent,
+
+      // Timings
+      mLapInvalidated: ((packet.data.sRaceStateFlags & 0x8) > 0),
+      mBestLapTime: numeral(packet.data.sBestLapTime).format("00:00:00"),
+      mLastLapTime: numeral(packet.data.sLastLapTime).format("00:00:00"),
+      mCurrentTime: numeral(packet.data.sCurrentTime).format("00:00:00"),
+      mEventTimeRemaining: numeral(packet.data.sEventTimeRemaining).format("00:00:00"),
+      mCurrentSector1Time: numeral(packet.data.sCurrentSector1Time).format("00:00:00"),
+      mCurrentSector2Time: numeral(packet.data.sCurrentSector2Time).format("00:00:00"),
+      mCurrentSector3Time: numeral(packet.data.sCurrentSector3Time).format("00:00:00"),
+      mFastestSector1Time: numeral(packet.data.sFastestSector1Time).format("00:00:00"),
+      mFastestSector2Time: numeral(packet.data.sFastestSector2Time).format("00:00:00"),
+      mFastestSector3Time: numeral(packet.data.sFastestSector3Time).format("00:00:00"),
+
+      // Car state
+      mAntiLockActive: ((packet.data.sRaceStateFlags & 0x16) > 0),
+      mBoostActive: ((packet.data.sRaceStateFlags & 0x32) > 0),
+    }
+  };
+
+  callback(telemetry);
+}
 
 let Packet = {
   "0": function(peek, callback) {
@@ -31,7 +70,7 @@ let Packet = {
         sFastestSector3Time: peek.payload.readFloatLE(68)
       }
     };
-    callback(packet);
+    decodeTelemetry(packet, callback);
   },
   "1": function(peek, callback) {
     callback({ header: peek.header, data: null });
